d808a1eaa7e8050f078350b8bbe092e9afbff3a3
Colin But <colinbut@users.noreply.github.com>
2018-10-20 22:24:30 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/spring/feign/example/springfeignexample/SpringFeignExampleApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.spring.feign.example.springfeignexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFeignExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFeignExampleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mycompany/spring/feign/example/springfeignexample/SpringFeignExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mycompany.spring.feign.example.springfeignexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringFeignExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
